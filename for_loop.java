c567aab8e9290a4d3d62903a1a419357090f1734
Manasi Chavan <manasisanjuchavan@gmail.com>
2021-07-07 08:41:06 +0530

    updated for loop with new algos

--- for_loop.java
+++ for_loop.java
@@ -1,7 +1,11 @@
 public class ForLoops{
 	public static void main(){
+
 	for(int i=1; i<=5; i++){
 		System.out.println("Hello for loop");
 	}
+	System.out.println("Done with for loops");
+	System.out.println("Wait what about while loop");
+
 }
 }
